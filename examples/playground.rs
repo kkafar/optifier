0f91e99362baafa1189e86a06969b65025010e54
Kacper Kafara <kacperkafara@gmail.com>
2026-01-14 22:18:58 +0100

    Implement `merge` function + remove invalid rustfmt configuration

--- examples/playground.rs
+++ examples/playground.rs
@@ -10,7 +10,11 @@ struct TestType {
 
 fn main() -> Result<(), ()> {
     let _complete = TestType { field_i32: 42, field_string: String::from("Hello world"), field_option_string: Some(String::from("Hello world")) };
-    let partial = TestTypePartial { field_i32: Some(42), field_string: Some(String::from("hello world")), field_option_string: Some(String::from("hello world")) };
-    dbg!(partial);
+    let partial = TestTypePartial { field_i32: None, field_string: Some(String::from("hello world")), field_option_string: None };
+    let second_partial = TestTypePartial { field_i32: Some(42), field_string: None, field_option_string: Some(String::from("field_option_string")) };
+    let merged = TestTypePartial::merge(partial, second_partial);
+
+
+    dbg!(merged);
     return Ok(());
 }
